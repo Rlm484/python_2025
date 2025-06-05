a5929e281342bc7393a7303d019c4f28e00ea01d
Rlm484 <rachaellmackinnon@gmail.com>
2025-06-06 09:59:31 +1000

    First Push

--- UNIT - 2.py
+++ UNIT - 2.py
@@ -1,3 +1,3 @@
 #Pseudocode
 #Pseudocode is like english/logic. We use it to express code in a more understandable manner.
-#Example: SET score TO 0, which translates to, score=0
+#Example: "SET score TO 0", which translates to, "score=0"
