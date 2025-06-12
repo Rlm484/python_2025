a4534c3394981b75e234c2058ca4eb0ac009d4bf
Rlm484 <rachaellmackinnon@gmail.com>
2025-06-12 12:04:32 +1000

    Unit 2

--- UNIT - 2.py
+++ UNIT - 2.py
@@ -1,3 +1,8 @@
 #Pseudocode
 #Pseudocode is like english/logic. We use it to express code in a more understandable manner.
 #Example: "SET score TO 0", which translates to, "score=0"
+# -Keep it clear and simple
+# -Use indentation to show structure
+# -Focus on logic, not syntax
+# -Comment your thoughts in plain English
+
