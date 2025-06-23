dc5a848b575cb02f57a44e407245d325431a6e10
Rlm484 <rachaellmackinnon@gmail.com>
2025-06-23 13:00:01 +1000

    push

--- prac3.1.py
+++ prac3.1.py
@@ -2,17 +2,23 @@ name = input("What is your name?: ")
 health = 100
 food = 50
 in_cave = True
-hours = int(input("How many hours did you wait in the cave?"))
+hours = int(input("How many hours did you wait in the cave? "))
 
-if health < 50 or food < 20
+if health < 50 or food < 20:
     print("Warning: You are weak!")
+
+if food < 0:
+    print("you died from hunger")
+
+if food < 0:
+    print("you died from a heart attack")
 
 health -= hours * 5
 food -= hours * 3
 
 print (f"{name}, your health is now {health} and you have {food} pieces of food left")
 
-if hours > 5:
+if hours > 5 and food > 0 and health > 0:
     print("You survived the night in the cave!")
 else:
     print("You left too early and got attacked")
