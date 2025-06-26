aab3c66f72610819d6440ed16a002d3d47347fb6
Rlm484 <rachaellmackinnon@gmail.com>
2025-06-26 12:19:03 +1000

    Update Challenge_A.py

--- Challenge_A.py
+++ Challenge_A.py
@@ -4,5 +4,7 @@ name = input("What's your name?: ")
 print(f"{name}? That's a nice name.")
 age = input("How old are you?: ")
 snack = input("Well, what snack do you want to bring?: ")
-game_length = float(input(f"{snack}? That sounds yum! But how long are we going to play? (in minutes): "))
+game_length = float(input(f"{snack}? That sounds yum! But how long are we going to play? (in minutes e.g. 15.5 OR 1): "))
 water_play = bool(input("Do you like water play? (Yes/No): "))
+
+print (f"Hi {name}! You're {age} years old and ready to play in Bluey's backyard. You'll play for {game_length} minute/s and bring your favourite snack, {snack}. Water play? If so you'll need to bring a towel? {water_play}! It looks like we'll be having a great day! ")
