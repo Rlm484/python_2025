28392a371b3f2a450d8892c4640fb94368825532
Rlm484 <rachaellmackinnon@gmail.com>
2025-06-26 12:08:41 +1000

    Update Challenge_A.py

---
+++ Challenge_A.py
@@ -0,0 +1,8 @@
+#Bluey's Backyard Adventure
+print("Hello, welcome to Bluey's backyard!")
+name = input("What's your name?: ")
+print(f"{name}? That's a nice name.")
+age = input("How old are you?: ")
+snack = input("Well, what snack do you want to bring?: ")
+game_length = float(input(f"{snack}? That sounds yum! But how long are we going to play? (in minutes): "))
+water_play = bool(input("Do you like water play? (Yes/No): "))
